8637038bd7472418ee15af01bfe782463f10f135
yankfy <yankfy@qq.com>
2020-11-22 23:57:02 +0800

    react+ts 2

--- react+ts/client/src/index.tsx
+++ react+ts/client/src/index.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-ReactDOM.render((
-    <h1>hello</h1>
-), document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Route, Redirect } from "react-router-dom";//三个路由组件
+import { Provider } from "react-redux";//负责把属性中的store传递给子组件
+import store from "./store";//引入仓库
+import { ConfigProvider } from "antd";//配置
+import zh_CN from "antd/lib/locale-provider/zh_CN";//国际化中文
+import "./assets/css/common.less";//通用的样式
+import Tabs from "./components/Tabs";//引入底部的页签导航
+import Home from "./routes/Home";//首页
+import Mine from "./routes/Mime";//我的课程
+import Profile from "./routes/Profile";//个人中心
+import { ConnectedRouter } from 'connected-react-router';//redux绑定路由
+import history from './store/history';
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <ConfigProvider locale={zh_CN}>
+                <main className="main-container">
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/mine" component={Mine} />
+                        <Route path="/profile" component={Profile} />
+                        <Redirect to="/" />
+                    </Switch>
+                </main>
+                <Tabs />
+            </ConfigProvider>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById("root")
+);
--- react+ts/client/webpack.config.js
+++ react+ts/client/webpack.config.js
@@ -3,35 +3,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const tsImportPluginFactory = require('ts-import-plugin')
 const path = require('path');
 module.exports = {
-    mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',//默认是开发模块
-    entry: './src/index.tsx',//设置入口文件
+    mode: process.env.NODE_ENV == 'production' ? 'production' : 'development', //默认是开发模块
+    entry: './src/index.tsx', //设置入口文件
     output: {
-        path: path.join(__dirname, 'dist'),//输出目录
-        filename: 'bundle.js'//输出的文件名
+        path: path.join(__dirname, 'dist'), //输出目录
+        filename: 'bundle.js' //输出的文件名
     },
-    devtool: 'source-map',//开发工具
+    devtool: 'source-map', //开发工具
     devServer: {
-        hot: true,//热更新插件
-        contentBase: path.join(__dirname, 'dist'),//静态文件根目录
+        hot: true, //热更新插件
+        contentBase: path.join(__dirname, 'dist'), //静态文件根目录
         historyApiFallback: { //browserHistory的时候，刷新会报404. 自动重定向到index.html
             index: './index.html'
         }
     },
-    resolve: {//配置如何解析
-        alias: {//别名
-            "@": path.resolve(__dirname, 'src'),// @指向src
-            "~": path.resolve(__dirname, 'node_modules')//~指向node_modules
+    resolve: { //配置如何解析
+        alias: { //别名
+            "@": path.resolve(__dirname, 'src'), // @指向src
+            "~": path.resolve(__dirname, 'node_modules') //~指向node_modules
         },
         //当你加载一个文件的时候,没有指定扩展名的时候，会自动寻找哪些扩展名
         extensions: ['.ts', '.tsx', '.js', '.json']
     },
     module: {
-        rules: [
-            {
-                test: /\.(j|t)sx?$/,//如果要加载的模块是jsx tsx js ts的话
-                loader: 'ts-loader',//使用ts-loader进行转译
+        rules: [{
+                test: /\.(j|t)sx?$/, //如果要加载的模块是jsx tsx js ts的话
+                loader: 'ts-loader', //使用ts-loader进行转译
                 options: {
-                    transpileOnly: true,//是否只转译
+                    transpileOnly: true, //是否只转译
                     //设置自定义转换器,TypeScript 可以将 TS 源码编译成 JS 代码，自定义转换器插件则可以让你定制生成的代码。比如删掉代码里的注释、改变变量的名字、将类转换为函数等等
                     //TypeScript 将 TS 代码编译到 JS 的功能，其实也是通过内置的转换器实现的，从 TS 2.3 开始，TS 将此功能开放，允许开发者编写自定义的转换器。
                     getCustomTransformers: () => ({
@@ -41,45 +40,46 @@ module.exports = {
                             "libraryDirectory": "es",
                             "style": "css"
                         })]
-                    }),//设置编译选项
+                    }), //设置编译选项
                     compilerOptions: {
-                        module: 'es2015'//模块规范是es2015
+                        module: 'es2015' //模块规范是es2015
                     }
                 },
             },
             {
-                test: /\.css$/,//css处理顺口
-                use: ['style-loader', {//style-loader是把CSS当作一个style标签插入到HTML中
-                    loader: 'css-loader',//css-loader是处理CSS中的import 和url
-                    options: { importLoaders: 0 }
-                }, {
-                        loader: 'postcss-loader',//postcss是用来给CSS中根据can i use 网站的数据添加厂商前缀的
+                test: /\.css$/, //css处理顺口
+                use: ['style-loader', { //style-loader是把CSS当作一个style标签插入到HTML中
+                        loader: 'css-loader', //css-loader是处理CSS中的import 和url
                         options: {
-                            plugins: [
-                                require('autoprefixer')
-                            ]
+                            importLoaders: 0
+                        }
+                    }, {
+                        loader: 'postcss-loader', //postcss是用来给CSS中根据can i use 网站的数据添加厂商前缀的
+                        options: {
+                            postcssOptions: (loader) => [require('autoprefixer')()]
                         }
                     },
                     {
-                        loader: 'px2rem-loader',//把px自动转成rem
+                        loader: 'px2rem-loader', //把px自动转成rem
                         options: {
-                            remUnit: 75,//一个rem代表75px
+                            remUnit: 75, //一个rem代表75px
                             remPrecesion: 8 //计算精度保留8位小数
                         }
-                    }]
+                    }
+                ]
             },
             {
-                test: /\.less$/,//处理less
+                test: /\.less$/, //处理less
                 use: ['style-loader', {
-                    loader: 'css-loader',
-                    options: { importLoaders: 0 }
-                },
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 0
+                        }
+                    },
                     {
                         loader: 'postcss-loader',
                         options: {
-                            plugins: [
-                                require('autoprefixer')
-                            ]
+                            postcssOptions: (loader) => [require('autoprefixer')()]
                         }
                     },
                     {
@@ -93,14 +93,14 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(jpg|png|gif|svg|jpeg)$/,//处理图片,把图片打包到输出目录中
+                test: /\.(jpg|png|gif|svg|jpeg)$/, //处理图片,把图片打包到输出目录中
                 use: ['url-loader']
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'//以此文件作为模板拼入打包后的文件并输出到目标目录中
+            template: './src/index.html' //以此文件作为模板拼入打包后的文件并输出到目标目录中
         }),
         //热更新插件
         new webpack.HotModuleReplacementPlugin()
